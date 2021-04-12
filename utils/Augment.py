1702cbad9a0c40a12b7e476bfa27cedbc337b1f0
Nikhil Bhamwani <52523591+nixxby@users.noreply.github.com>
2021-04-13 00:09:22 +0530

    Added batch-wise augment for whole dataset

--- utils/Augment.py
+++ utils/Augment.py
@@ -19,48 +19,49 @@ testX = testX.astype('float32')
 trainX/=255
 testX/=255
 
-datagen = ImageDataGenerator()
-datagen.fit(trainX)
-for batchX, batchy in datagen.flow(trainX, trainy, batch_size=9, seed=99):
-# plot first few images
-  for i in range(9):
-    # define subplot
-    pyplot.subplot(330 + 1 + i)
-    # plot raw pixel data
-    pyplot.imshow(batchX[i])
-  # show the figure
-  pyplot.show()
-  break
+def augment_data( trainX, trainy, p=0.25, batch_size=32):
+  num_choices = int(p*trainX.shape[0])
   
-choices = int(p*trainX.shape[0])
-indices = np.sort(np.random.choice(trainX.shape[0], choices, replace=False))
-print(indices)
-datagen = ImageDataGenerator(        
-            rotation_range=90,
-            width_shift_range=0.2,  
-            height_shift_range=0.2,    
-            shear_range=0.2,        
-            zoom_range=0.2,        
-            horizontal_flip=True,         
-            fill_mode='nearest', cval=125)
-tempX = np.zeros((choices,32,32,3))
-tempy = np.zeros((choices,1))
-c=0
-for x in range(trainX.shape[0]):
-  if x in indices:
-    tempX[c] = trainX[x:x+1]
-    tempy[c] = trainy[x:x+1]
-    c=c+1
+  #Selecting random image indices to be augmented in each step.
+  indices = np.zeros((8,num_choices))
+  for i in range(8):
+    indices[i] = np.sort(np.random.choice(trainX.shape[0], num_choices, replace=False))
+  indices= indices. astype(int)
 
-c=0
-for batchX,batchy in datagen.flow(tempX,tempy, batch_size=9):    
-  for i in range(9):
-    pyplot.subplot(330 + 1 + i)
-    if i in indices:
-      pyplot.imshow(batchX[c])
-      c=c+1
-    else:
-      pyplot.imshow(trainX[i])
-  pyplot.show()
-  break
+  total_iterations = int(trainX.shape[0] / batch_size) + 1
   
+  #Augmentation Pipeline: composed of 8 steps.
+  for i in range(8):
+    num_augment = np.zeros(8).astype(int)
+    num_augment[i] = 1
+    datagen = ImageDataGenerator( rotation_range=150*num_augment[0],
+                                  horizontal_flip=bool(num_augment[1]), 
+                                  vertical_flip = bool(num_augment[2]),
+                                  width_shift_range=0.3* num_augment[3],
+                                  height_shift_range=0.3* num_augment[4],
+                                  shear_range=0.3* num_augment[5],
+                                  zoom_range=0.3* num_augment[6],
+                                  samplewise_std_normalization= bool(num_augment[7]),
+                                  fill_mode='nearest')
+    
+    for iter in range(total_iterations):
+      #temporarily Storing images to be augmented in this ndarray in 
+      # for each iteration
+      tempX = np.zeros((batch_size,32,32,3))
+      tempy = np.zeros((batch_size,1))
+      start_index = batch_size * iter
+      end_index = start_index + batch_size
+
+      for x in range(start_index, end_index):
+        if x in indices[i]:
+          tempX[x%batch_size] = trainX[x:x+1]
+          tempy[x%batch_size] = trainy[x:x+1]
+      #Applying datagen on temp images to be augmented, in each iteration.
+      datagen.fit(tempX)
+      for batchX,batchy in datagen.flow(tempX,tempy, batch_size,shuffle=False):    
+        for x in range(start_index, end_index):
+          if x in indices[i]:
+            trainX[x] = batchX[x%batch_size]
+        break
+
+  return trainX, trainy
